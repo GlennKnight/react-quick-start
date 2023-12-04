45d2925d46832920feaf031805189686279d9422
GlennKnight <48197163+GlennKnight@users.noreply.github.com>
2023-12-04 21:14:30 +0900

    Wrapping up: 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 
-function Square({ value, onSquareClick }) {
+function Square({ value, hightlight, onSquareClick }) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button className="square" style={{ color: hightlight ? 'red' : null }} onClick={onSquareClick}>
       {value}
     </button>
   );
@@ -10,7 +10,7 @@ function Square({ value, onSquareClick }) {
 
 function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
+    if (squares[i] || calculateWinner(squares).winner) {
       return;
     }
     const nextSquares = squares.slice();
@@ -22,10 +22,12 @@ function Board({ xIsNext, squares, onPlay }) {
     onPlay(nextSquares);
   }
 
-  const winner = calculateWinner(squares);
+  const { winner, line } = calculateWinner(squares);
   let status;
   if (winner) {
     status = 'Winner: ' + winner;
+  } else if (squares.every(square => square)) {
+    status = 'Draw';
   } else {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
@@ -35,7 +37,8 @@ function Board({ xIsNext, squares, onPlay }) {
     const row = [];
     for (let ixCol = 0; ixCol < 3; ixCol++) {
       const ix = 3 * ixRow + ixCol;
-      row.push(<Square key={ix} value={squares[ix]} onSquareClick={() => handleClick(ix)} />);
+      const higlight = winner && line.includes(ix);
+      row.push(<Square key={ix} value={squares[ix]} hightlight={higlight} onSquareClick={() => handleClick(ix)} />);
     }
     board.push(<div key={ixRow} className="board-row">{row}</div>);
   }
@@ -113,8 +116,8 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return { winner: squares[a], line: lines[i] };
     }
   }
-  return null;
+  return { winner: null, line: null };
 }
