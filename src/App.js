542a6efe6e4e84a3c1ba442534d2baf8267eb139
GlennKnight <48197163+GlennKnight@users.noreply.github.com>
2023-12-04 20:08:29 +0900

    Wrapping up: 3

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [descending, setDescending] = useState(true);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
@@ -83,12 +84,15 @@ export default function Game() {
     );
   });
 
+  if (!descending) moves.reverse();
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
+        <button onClick={() => setDescending(!descending)}>{descending ? 'Descending' : 'Ascending'}</button>
         <ol>{moves}</ol>
       </div>
     </div>
